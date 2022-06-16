a2483528589e59aeeeb93674d9e1ffb1a32c6361
Lackier <dan23061999@mail.ru>
2022-06-16 22:35:02 +0300

    init

---
+++ src/main/java/com/lackier/monetka/back/MonetkaBackApp.java
@@ -0,0 +1,11 @@
+package com.lackier.monetka.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetkaBackApp {
+	public static void main(String[] args) {
+		SpringApplication.run(MonetkaBackApp.class, args);
+	}
+}
