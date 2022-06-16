8460c32185068b6c1c9b1d61205c8699ede7a068
Lackier <dan23061999@mail.ru>
2022-06-16 22:43:41 +0300

    init-2

---
+++ src/main/java/com/lackier/monetka/back/domain/Category.kt
@@ -0,0 +1,10 @@
+package com.lackier.monetka.back.domain
+
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "category")
+class Category : AbstractEntity() {
+
+}
